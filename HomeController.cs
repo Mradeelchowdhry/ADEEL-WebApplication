9a4bdbc97381ad5e3a96bf6227d3b02b6162b2ff
Mradeelchowdhry <140377634+Mradeelchowdhry@users.noreply.github.com>
2024-06-14 12:54:54 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,22 @@
+﻿using ADEEL_WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ADEEL_WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+     
+    }
+}
---
+++ ShopController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADEEL_WebApplication.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            TempData.Keep("pname");
+            TempData.Keep("desc"); 
+            TempData.Keep("price");
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View("Add");
+        }
+
+        [HttpPost]
+        public IActionResult Add(string pname, string desc, string price)
+        {
+            if (pname != String.Empty && desc != string.Empty && price != string.Empty)
+            {
+                ViewData["pname"] = pname;
+                ViewData["desc"] = desc;
+                ViewData["price"] = price;
+
+                ViewBag.pname = pname;
+                ViewBag.desc = desc;
+                ViewBag.price = price;
+
+                TempData["pname"] = pname;
+                TempData["desc"] = desc;
+                TempData["price"] = price;
+            }
+
+            return View("Add");
+            //return RedirectToAction("Shop");
+        }
+    }
+}
